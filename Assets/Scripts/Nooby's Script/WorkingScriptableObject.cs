fccf1e9f7d421f49e4e227b2cc3b2d6f2fec934c
JH-AFK <NGJH.22@ichat.sp.edu.sg>
2023-09-14 22:24:54 +0800

    Created ScriptableObject

---
+++ Assets/Scripts/Nooby's Script/WorkingScriptableObject.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Asset", menuName =("Create SO"))]
+public class WorkingScriptableObject : ScriptableObject
+{
+    public int timer;
+}
