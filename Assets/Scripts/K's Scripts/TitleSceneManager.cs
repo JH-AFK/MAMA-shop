3bdb961aa3ff9f678e7e7761d9293c45d3251137
Account-95555 <acc95555@gmail.com>
2023-09-18 00:21:26 +0800

    added (unused) cursor and some feedback to buttons

--- Assets/Scripts/K's Scripts/TitleSceneManager.cs
+++ Assets/Scripts/K's Scripts/TitleSceneManager.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class TitleSceneManager : MonoBehaviour
 {
+    public AudioSource BGM;
+    public AudioClip clickSFX;
+    public AudioClip meowSFX;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,22 +24,42 @@ public class TitleSceneManager : MonoBehaviour
 
     public void PlayButton()
     {
-
+        ButtonClickFeedback();
+        DisableButton();
     }
     
     public void SettingsButton()
     {
-
+        ButtonClickFeedback();
+        DisableButton();
     }
 
     public void CreditsButton()
     {
-
+        ButtonClickFeedback();
+        DisableButton();
     }
 
     public void QuitButton()
     {
+        ButtonClickFeedback();
+        DisableButton();
         Application.Quit(); //Will make the quitting better
+    }
+
+    void ButtonClickFeedback()
+    {
+        BGM.PlayOneShot(clickSFX);
+        BGM.PlayOneShot(meowSFX);
+    }
+
+    void DisableButton() //prevents button from getting spammed after click
+    {
+        if (EventSystem.current.currentSelectedGameObject.GetComponent<Button>().enabled)
+        {
+            EventSystem.current.currentSelectedGameObject.GetComponent<Button>().enabled = false;
+        }
+        
     }
 
 }
