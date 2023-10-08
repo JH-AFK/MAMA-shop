160c0851267fc7d0c8d0e638484b43920676a2dc
JH-AFK <NGJH.22@ichat.sp.edu.sg>
2023-10-09 00:13:31 +0800

    change the input amount so that it only accepts numbers not letters

--- Assets/Scripts/K's Scripts/RestockPanel.cs
+++ Assets/Scripts/K's Scripts/RestockPanel.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
@@ -8,11 +9,14 @@ public class RestockPanel : MonoBehaviour
 {
     public ItemSO currentItem;
     public TextMeshProUGUI itemDescription;
+    public TMP_InputField enterAmmountText;
     public Image itemImage;
     public AudioSource BGM;
     public AudioClip feedbackAudio;
     public GameObject restockPanel;
     public GameObject buyPanel;
+    public float amount;
+    public string inital;
 
     void Start()
     {
@@ -32,7 +36,7 @@ public class RestockPanel : MonoBehaviour
         {
             itemImage.enabled = false;
             buyPanel.SetActive(false);
-        }
+        }//this should be made a function and not put in an update but it is fine where it is rn
     }
 
     public void RestockClicked()
@@ -56,4 +60,23 @@ public class RestockPanel : MonoBehaviour
         currentItem = item;
     }
 
+    public void OnTyped(TextMeshProUGUI startingText)
+    {
+        inital = startingText.text;
+        startingText.text = string.Empty;
+    }
+
+    public void OnReleased(TextMeshProUGUI startingText)
+    {
+        if(int.TryParse(enterAmmountText.text,out int result))
+        {
+            amount = result;
+        }
+        else
+        {
+            Debug.Log("pls input numbers, not letters");
+            startingText.text = inital;
+            enterAmmountText.text = string.Empty;
+        }
+    }
 }
